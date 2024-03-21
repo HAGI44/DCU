b68863fdbed63c1f533d274613aa96ac61ff7391
HAGI44 <gmltn1052@gmail.com>
2024-03-21 11:14:22 +0900

    메인페이지 학생정보 표시 기능 추가중

--- cpst3/src/main/java/com/appl3/cpst3/domain/entity/Student.java
+++ cpst3/src/main/java/com/appl3/cpst3/domain/entity/Student.java
@@ -45,4 +45,115 @@ public class Student {
     public boolean isPreGraduation() {
         return this.preGraduation;
     }
+    
+    private int admissionYear; // 개설년도
+    private int admissionSemester; // 개설학기
+    private int acquiredCredits; // 취득기준 학점
+    private boolean academicWarning; // 학사경고
+    private int remainingCredits; // 남은학점
+    private int additionalCredits; // 추가된 수강가능 학점
+    private int totalCredits; // 수강가능 학점
+    private int appliedCredits; // 신청학점
+    private int remainingMileage; // 잔여 마일리지
+    private int appliedMileage; // 신청 마일리지
+    
+    // 개설년도에 대한 getter 메서드
+    public int getAdmissionYear() {
+        return admissionYear;
+    }
+
+    // 개설년도에 대한 setter 메서드
+    public void setAdmissionYear(int admissionYear) {
+        this.admissionYear = admissionYear;
+    }
+
+    // 개설학기에 대한 getter 메서드
+    public int getAdmissionSemester() {
+        return admissionSemester;
+    }
+
+    // 개설학기에 대한 setter 메서드
+    public void setAdmissionSemester(int admissionSemester) {
+        this.admissionSemester = admissionSemester;
+    }
+
+    // 취득기준 학점에 대한 getter 메서드
+    public int getAcquiredCredits() {
+        return acquiredCredits;
+    }
+
+    // 취득기준 학점에 대한 setter 메서드
+    public void setAcquiredCredits(int acquiredCredits) {
+        this.acquiredCredits = acquiredCredits;
+    }
+
+    // 학사경고 여부에 대한 getter 메서드
+    public boolean isAcademicWarning() {
+        return academicWarning;
+    }
+
+    // 학사경고 여부에 대한 setter 메서드
+    public void setAcademicWarning(boolean academicWarning) {
+        this.academicWarning = academicWarning;
+    }
+
+    // 남은학점에 대한 getter 메서드
+    public int getRemainingCredits() {
+        return remainingCredits;
+    }
+
+    // 남은학점에 대한 setter 메서드
+    public void setRemainingCredits(int remainingCredits) {
+        this.remainingCredits = remainingCredits;
+    }
+
+    // 추가된 수강가능 학점에 대한 getter 메서드
+    public int getAdditionalCredits() {
+        return additionalCredits;
+    }
+
+    // 추가된 수강가능 학점에 대한 setter 메서드
+    public void setAdditionalCredits(int additionalCredits) {
+        this.additionalCredits = additionalCredits;
+    }
+
+    // 수강가능 학점에 대한 getter 메서드
+    public int getTotalCredits() {
+        return totalCredits;
+    }
+
+    // 수강가능 학점에 대한 setter 메서드
+    public void setTotalCredits(int totalCredits) {
+        this.totalCredits = totalCredits;
+    }
+
+    // 신청학점에 대한 getter 메서드
+    public int getAppliedCredits() {
+        return appliedCredits;
+    }
+
+    // 신청학점에 대한 setter 메서드
+    public void setAppliedCredits(int appliedCredits) {
+        this.appliedCredits = appliedCredits;
+    }
+
+    // 잔여 마일리지에 대한 getter 메서드
+    public int getRemainingMileage() {
+        return remainingMileage;
+    }
+
+    // 잔여 마일리지에 대한 setter 메서드
+    public void setRemainingMileage(int remainingMileage) {
+        this.remainingMileage = remainingMileage;
+    }
+
+    // 신청 마일리지에 대한 getter 메서드
+    public int getAppliedMileage() {
+        return appliedMileage;
+    }
+
+    // 신청 마일리지에 대한 setter 메서드
+    public void setAppliedMileage(int appliedMileage) {
+        this.appliedMileage = appliedMileage;
+    }
 }
--- cpst3/src/main/java/com/appl3/cpst3/repository/StudentRepository.java
+++ cpst3/src/main/java/com/appl3/cpst3/repository/StudentRepository.java
@@ -4,6 +4,6 @@ import com.appl3.cpst3.domain.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    // 학생 코드로 학생을 조회하는 메서드
+    // 학번으로 학생을 조회하는 메서드
     Student findByStudentCode(String studentCode);
 }
